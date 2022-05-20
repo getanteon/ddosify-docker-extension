f5a640b412e5bdd4ff4ba17ae18048396b410d87
fatihbaltaci <baltacifatih14@gmail.com>
2022-05-20 17:08:22 +0300

    Update app js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,61 @@
 import { DockerMuiThemeProvider } from "@docker/docker-mui-theme";
 import { createDockerDesktopClient } from "@docker/extension-api-client";
-import { Grid, TextField } from "@mui/material";
+import {
+  FormControl,
+  FormGroup,
+  Grid,
+  InputLabel,
+  Select,
+  TextField,
+} from "@mui/material";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import MenuItem from "@mui/material/MenuItem";
+
+const protocols = [
+  {
+    value: "https",
+    label: "HTTPS",
+  },
+  {
+    value: "http",
+    label: "HTTP",
+  },
+];
+
+const methods = [
+  {
+    value: "GET",
+    label: "GET",
+  },
+  {
+    value: "POST",
+    label: "POST",
+  },
+  {
+    value: "PUT",
+    label: "PUT",
+  },
+  {
+    value: "PATCH",
+    label: "PATCH",
+  },
+  {
+    value: "DELETE",
+    label: "DELETE",
+  },
+  {
+    value: "HEAD",
+    label: "HEAD",
+  },
+  {
+    value: "OPTIONS",
+    label: "OPTIONS",
+  },
+];
+
 const client = createDockerDesktopClient();
 
 function useDockerDesktopClient() {
@@ -47,6 +98,10 @@ function App() {
           options.request_count,
           "-d",
           options.duration,
+          "-p",
+          options.protocol,
+          "-m",
+          options.method,
         ],
         {
           stream: {
@@ -75,10 +130,7 @@ function App() {
   }, [running]);
 
   const stopDdosify = async () => {
-    ddClient.extension.vm.cli.exec(
-      "killall",
-      ["-SIGINT", "ddosify"]
-    );
+    ddClient.extension.vm.cli.exec("killall", ["-SIGINT", "ddosify"]);
   };
 
   const clearEmoji = (str) => {
@@ -96,85 +148,150 @@ function App() {
     <DockerMuiThemeProvider>
       <CssBaseline />
       <div className="App">
-        <Button
-          variant="contained"
-          onClick={() => setRunning(true)}
-          disabled={running}
-        >
-          Run Ddosify
-        </Button>
-        <Button
-          variant="contained"
-          color="error"
-          onClick={stopDdosify}
-          disabled={!running}
-        >
-          Stop
-        </Button>
-
         <Grid
           container
-          owSpacing={1}
-          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          style={{ marginTop: "1rem" }}
+          columnSpacing={{ xs: 1 }}
+          rowSpacing={2}
+          style={{ marginTop: "5rem" }}
         >
-          <Grid item>
-            <TextField
-              required
-              variant="filled"
-              label="URL"
-              value={options?.target}
-              onChange={(e) =>
-                setOptions((prevState) => ({
-                  ...prevState,
-                  target: e.target.value,
-                }))
-              }
-            />
+          <Grid container item>
+            <Grid item xs={1}>
+              <TextField
+                style={{ width: "100%", textAlign: "left" }}
+                select
+                value={options?.method}
+                onChange={(e) =>
+                  setOptions((prevState) => ({
+                    ...prevState,
+                    method: e.target.value,
+                  }))
+                }
+                helperText="Method"
+              >
+                {methods.map((method) => (
+                  <MenuItem key={method.value} value={method.value}>
+                    {method.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid item xs={0.2}></Grid>
+            <Grid item xs={10.8} container>
+              <Grid item xs={1}>
+                <TextField
+                  style={{ width: "100%", textAlign: "left" }}
+                  select
+                  value={options?.protocol}
+                  onChange={(e) =>
+                    setOptions((prevState) => ({
+                      ...prevState,
+                      protocol: e.target.value,
+                    }))
+                  }
+                  helperText="Protocol"
+                >
+                  {protocols.map((protocol) => (
+                    <MenuItem key={protocol.value} value={protocol.value}>
+                      {protocol.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid item xs={11}>
+                <TextField
+                  style={{ width: "100%" }}
+                  required
+                  variant="outlined"
+                  placeholder="example.com"
+                  helperText="Target URL"
+                  value={options?.target}
+                  onChange={(e) =>
+                    setOptions((prevState) => ({
+                      ...prevState,
+                      target: e.target.value,
+                    }))
+                  }
+                />
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item>
-            <TextField
-              required
-              variant="filled"
-              label="Request Count"
-              type="number"
-              value={options?.request_count}
-              onChange={(e) =>
-                setOptions((prevState) => ({
-                  ...prevState,
-                  request_count: e.target.value,
-                }))
-              }
-            />
+
+          <Grid item container columnSpacing={{ xs: 2 }}>
+            <Grid item>
+              <TextField
+                required
+                variant="filled"
+                label="Request Count"
+                type="number"
+                value={options?.request_count}
+                onChange={(e) =>
+                  setOptions((prevState) => ({
+                    ...prevState,
+                    request_count: e.target.value,
+                  }))
+                }
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                required
+                variant="filled"
+                label="Duration (s)"
+                type="number"
+                value={options?.duration}
+                onChange={(e) =>
+                  setOptions((prevState) => ({
+                    ...prevState,
+                    duration: e.target.value,
+                  }))
+                }
+              />
+            </Grid>
           </Grid>
-          <Grid item>
-            <TextField
-              required
-              variant="filled"
-              label="Duration (s)"
-              type="number"
-              value={options?.duration}
-              onChange={(e) =>
-                setOptions((prevState) => ({
-                  ...prevState,
-                  duration: e.target.value,
-                }))
-              }
-            />
-          </Grid>
-        </Grid>
-        <Grid container style={{ marginTop: "3rem" }}>
-          <pre
-            style={{
-              "text-align": "left",
-              backgroundColor: "#323028",
-              border: "3px solid #999",
-              padding: "20px",
-              width: "100%",
-            }}
+          <Grid container></Grid>
+
+          <Grid
+            item
+            container
+            columnSpacing={{ xs: 2 }}
+            justifyContent="flex-start"
+            style={{ marginTop: "3rem" }}
           >
-            {backendInfo}
-          </pre>
+            <Grid item>
+              <Button
+                size="large"
+                variant="contained"
+                onClick={() => setRunning(true)}
+                disabled={running}
+              >
+                Run Ddosify
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                size="large"
+                variant="contained"
+                color="error"
+                onClick={stopDdosify}
+                disabled={!running}
+              >
+                Stop
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid item container style={{ marginTop: "3rem" }}>
+            <pre
+              style={{
+                textAlign: "left",
+                backgroundColor: "#323028",
+                border: "3px solid #999",
+                padding: "20px",
+                width: "100%",
+              }}
+            >
+              {backendInfo}
+            </pre>
+          </Grid>
         </Grid>
       </div>
     </DockerMuiThemeProvider>
